788972863635c908faa247e874b5fc01e169c4cd
KvRae <karamelmannai@gmail.com>
2022-10-03 22:54:19 +0100

    updating gestemp

--- src/main/java/rest/ressources/GestionEmploye.java
+++ src/main/java/rest/ressources/GestionEmploye.java
@@ -3,6 +3,8 @@ package rest.ressources;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -15,13 +17,17 @@ import rest.entities.Employe;
 @Path("employes")
 @Api
 public class GestionEmploye {
-	
-	public static  List<Employe> employes=new ArrayList<Employe>();
 
-	
+	public static  List<Employe> employes=new ArrayList<Employe>();
+	GenericEntity<List<Employe>> entity = new GenericEntity<List<Employe>>(employes) {};
+
+
 	@POST
-	@Consumes("application/json")
-	@Produces("text/plain")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(value = "Post an epmploye")
+	@ApiResponses({
+			@ApiResponse(code=200, message="Success")
+	})
 	public Response ajouterEmploye(Employe employe) {
 		 if(employes.add(employe))
 	 		return Response.status(Status.CREATED).entity("Add Successful").build();
@@ -30,7 +36,7 @@ public class GestionEmploye {
 		
 	}
 	@GET
-	@Produces("text/plain")
+	@Produces("text/xml")
 	@ApiOperation(value = "Get all employees")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "Success"),
@@ -59,34 +65,41 @@ public class GestionEmploye {
 		
 		
 	}
-	
-		
+
+
 	@PUT
-	@Consumes("application/xml")
-	@Produces("text/plain")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(value = "update an Employe")
+	@ApiResponses({
+			@ApiResponse(code=200, message="Success")
+	})
 	public Response updateEmploye(Employe e) {
 		int index= this.getIndexByCin(e.getCin());
 		if (index!=-1) {
 			employes.set(index, e);
 			return Response.status(Status.OK).entity("update successful").build();
-			
+
 		}
 		return Response.status(Status.NOT_FOUND).entity("update unsuccessful").build();
-	
+
 	}
-	
+
 	@DELETE
-	@Path("{id}")
-	public boolean deleteEmpl(@PathParam(value = "id") int cin){
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(value = "Delete an Employe")
+	@ApiResponses({
+			@ApiResponse(code=200, message="Success")
+	})
+	public boolean deleteEmpl(int cin){
 		int index= getIndexByCin(cin);
-		
+
 		if (index!=-1) {
 			employes.remove(index);
 			return true;
-		}else 
+		}else
 			return false;
-			
-    }
+
+	}
 	
 	public int getIndexByCin(int cin) {
 		for(Employe emp: employes) {
